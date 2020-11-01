d31eb0ed82d88dfe87fc4be18d02974719fdcf8f
Vyas Ramankulangara <vyas01890@outlook.com>
2020-11-01 13:23:49 -0600

    fix grid

--- src/components/Tutors/Tutor.js
+++ src/components/Tutors/Tutor.js
@@ -9,13 +9,15 @@ const Tutor = ({ tutors }) => {
             <Row>
                 <Col>
                     <div className='tutors'>
-                        <div>
+                        <div className="tutorName">
                            <img style={{marginRight:'10px', marginBottom: '10px'}} src={LinkedIn}></img>
                            {tutors.prioritize ? <span style={{color: '#ECD00A' }}>{tutors.name}</span> : <span>{tutors.name}</span>}
                         </div>
-                        {tutors.courses.map((course, index) => (
-                            <div class="cardSubjects">{course}</div>
-                        ))}
+                        <div className="tutorCourse">
+                            {tutors.courses.map((course, index) => (
+                                <div class="cardSubjects">{course}</div>
+                            ))}
+                        </div>
                     </div>
                 </Col>
             </Row>
