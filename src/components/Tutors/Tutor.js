590723976da7784ad142f79da5869221a7bc4c30
Muhammad Usman <mohammadusmanxero@gmail.com>
2020-10-31 21:07:47 -0500

    carousel

--- src/components/Tutors/Tutor.js
+++ src/components/Tutors/Tutor.js
@@ -3,7 +3,6 @@ import { Col, Row } from "react-bootstrap";
 import "./Tutor.css";
 import LinkedIn from "../../assets/LinkedIn.svg";
 
-<<<<<<< HEAD
 const Tutor = ({ tutors }) => {
   return (
     <>
@@ -11,40 +10,28 @@ const Tutor = ({ tutors }) => {
         <Col>
           <div className="tutors">
             <div>
-              <a
-                style={{ alignItems: "left", marginRight: "10px" }}
-                href={tutors.linkedin}
-              >
-                <img src={LinkedIn}></img>
-              </a>
-              {tutors.name}
+              <img
+                style={{
+                  alignItems: "left",
+                  marginRight: "10px",
+                  marginBottom: "10px",
+                }}
+                src={LinkedIn}
+              ></img>
+              {tutors.prioritize ? (
+                <span style={{ color: "#ECD00A" }}>{tutors.name}</span>
+              ) : (
+                <span>{tutors.name}</span>
+              )}
             </div>
-            {tutors.courses}
+            {tutors.courses.map((course, index) => (
+              <div class="cardSubjects">{course}</div>
+            ))}
           </div>
         </Col>
       </Row>
     </>
   );
-=======
-const Tutor = ({ tutors }) => { 
-    return (
-       <>
-            <Row>
-                <Col>
-                    <div className='tutors'>
-                        <div>
-                           <img style={{alignItems: 'left', marginRight:'10px', marginBottom: '10px'}} src={LinkedIn}></img>
-                           {tutors.prioritize ? <span style={{color: '#ECD00A' }}>{tutors.name}</span> : <span>{tutors.name}</span>}
-                        </div>
-                        {tutors.courses.map((course, index) => (
-                            <div class="cardSubjects">{course}</div>
-                        ))}
-                    </div>
-                </Col>
-            </Row>
-       </>
-    );
->>>>>>> 3998a92163b016b4a554b5cc7f35efc99ced682e
 };
 
 export default Tutor;
--- src/pages/Tutoring/Tutoring.js
+++ src/pages/Tutoring/Tutoring.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Tutoring1 from "../../assets/Tutoring.svg";
 import discordIcon from "../../assets/discordIcon.svg";
@@ -6,11 +6,24 @@ import tutors from "../../tutors";
 import Tutor from "../../components/Tutors/Tutor";
 import { Col, Container, Row } from "react-bootstrap";
 import "./Tutoring.css";
+import Carousel from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
 
 const Tutoring = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 650);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
   return (
     <div>
-      <Container fluid style={{ padding: "2em" }}>
+      <Container fluid>
         <Row>
           <Col md="6">
             <div className="order-md-0" className="tutoring">
@@ -27,7 +40,6 @@ const Tutoring = () => {
               </p>
             </div>
             <div>
-<<<<<<< HEAD
               <Button
                 variant=""
                 target="_blank"
@@ -44,65 +56,6 @@ const Tutoring = () => {
                 Become a Tutor
               </Button>
               <div></div>
-=======
-				<Container fluid style={{ padding: '2em' }}>
-					<Row>
-                        <Col md='6' className='order-md-2'>
-                            <img src={Tutoring2} 
-                                alt='Tutoring 2 svg' 
-                                className='img-fluid'
-                                >
-                            </img>
-                            <img src={Tutoring1} 
-                                alt='Tutoring 2 svg' 
-                                className='img-fluid'
-                            /> 
-                        </Col>
-                        <Col>
-                            <div md='6' className='order-md-0' className='tutoring'>
-								<h1>Tutoring</h1>
-								<p style={{textAlign: "justify", maxWidth: 470}}>
-                                    CougarCS offers weekly tutoring time slots for many of our
-                                    degree's toughest classes. If you need help in your
-                                    coursework, please do not hesitate to reach out to our tutoring
-                                    team! Email <a href='mailto:tutoring@CougarCS.com?subject=CougarCS Tutor Application'>tutoring@CougarCS.com</a> with your course code
-                                    (COSC XXXX) in the subject line and our tutors will contact you
-                                    within 24 - 48 hours.
-								</p>
-							</div>
-                            <div>
-                                <Button variant='' target='_blank' className='DiscordButton' onClick={(e) => {e.preventDefault();
-                                    window.open('https://discord.gg/eXMqUz', '_blank')}}>
-                                        <img src={discordIcon} className='DiscordIcon'></img> Join Discord</Button>
-                                <Button variant="outline-danger" className='TutorButton'>Become a Tutor</Button>{' '}
-                            </div>
-						</Col>
-                    </Row>
-				</Container>
-                <Container>
-                    <h1 style={{ marginTop: '1.8em', textAlign: 'center' }}>Tutoring Available For</h1>
-					<Row>
-						<div>
-							<div className='row1'><p>COSC 1360</p></div>
-                            <div className='row1'><p>COSC 1430</p></div>
-                            <div className='row1'><p>COSC 2430</p></div>
-                            <br/>
-                            <div className='row2'><p>COSC 3360</p></div>
-                            <div className='row2'><p>COSC 3380</p></div>
-						</div>
-					</Row>
-                </Container>
-                <Container fluid className='tutoringCard'>
-                    <h1 style={{ marginTop: '1.8em' }}>Tutoring is Possible Because of</h1>
-					<Row className='cnt'>
-						<div className='officerImages'>
-							{tutors.map((tutor) =>
-								<Tutor tutors={tutor} key={tutor.id} />
-							)}
-						</div>
-					</Row>
-                </Container>
->>>>>>> 3998a92163b016b4a554b5cc7f35efc99ced682e
             </div>
           </Col>
           <Col md="6" className="order-md-2">
@@ -120,12 +73,20 @@ const Tutoring = () => {
         </div>
 
         <h3 className="my-4 text-center">Tutoring is Possible Because of</h3>
-        <Row className="cnt">
-          <div className="officerImages">
-            {tutors.map((tutor) => (
-              <Tutor tutors={tutor} key={tutor.id} />
-            ))}
-          </div>
+        <Row>
+          {isDesktop ? (
+            <div className="officerImages">
+              {tutors.map((tutor) => (
+                <Tutor tutors={tutor} key={tutor.id} />
+              ))}
+            </div>
+          ) : (
+            <Carousel>
+              {tutors.map((tutor) => (
+                <Tutor tutors={tutor} key={tutor.id} />
+              ))}
+            </Carousel>
+          )}
         </Row>
       </Container>
     </div>
